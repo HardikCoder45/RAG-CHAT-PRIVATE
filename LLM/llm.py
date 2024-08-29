7c9edc0658f815fedb2446256d077d8ac546c5ee
Hardik Arora <119507131+HardikCoder45@users.noreply.github.com>
2024-08-29 10:18:46 +0530

    Update llm.py

--- LLM/llm.py
+++ LLM/llm.py
@@ -2,24 +2,25 @@ from langchain_groq import ChatGroq
 from langchain_openai import ChatOpenAI
 from langchain_mistralai import ChatMistralAI
 import json
+import os
 
 with open("models.json", 'r') as file:
     data = json.load(file)
 models =data['models']
-     
+api_key=os.getenv("GROQ_API_KEY")
 
 class Models:
     def __init__( self):
        pass
      
     def llama(self):
-       self.llama = ChatGroq(model=models[0]['model'],api_key=models[0]['api-key'])
+       self.llama = ChatGroq(model=models[0]['model'],api_key=api_key)
        return self.llama
     def mistral(self):
        self.mistral = ChatMistralAI(model=models[1]['model'],api_key=models[1]['api-key'])
        return self.mistral
     def gemma(self):
-       self.gemma = ChatGroq(model=models[2]['model'],api_key=models[2]['api-key'])
+       self.gemma = ChatGroq(model=models[2]['model'],api_key=api_key)
        return self.gemma
     
     
